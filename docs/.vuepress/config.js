9bf9814a81f8122de3a3b3e5c494ab2d31538587
zhifu <zhifu@yunshan.net>
2022-07-25 18:26:42 +0800

    Add TDK and delete notes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,28 +1,19 @@
-/**
- * 提示：如您想使用JS版本的配置文件可参考：https://github.com/xugaoyi/vuepress-theme-vdoing/tree/a2f03e993dd2f2a3afdc57cf72adfc6f1b6b0c32/docs/.vuepress
- */
 const { resolve } = require("path");
 const json5 = require('json5')
-// import { defineConfig4CustomTheme, UserPlugins } from "vuepress/config";
-// import { VdoingThemeConfig } from "vuepress-theme-vdoing/types";
 const dayjs = require("dayjs");
 
 module.exports = ({
   theme: resolve(__dirname, "../../vdoing"), // 使用本地主题包
   dest: 'dist',
 
-  // patterns: ['**/*.md', '**/*.vue', '**/*.json', '**/*.txt'],
-
   locales: {
     "/": {
       lang: "en-US",
-      title: "DeepFlow (Community) Docs",
-      description: "",
+      title: "DeepFlow Docs",
     },
     '/zh/': {
       lang: 'zh-CN',
-      title: 'DeepFlow (Community) 文档',
-      description: ''
+      title: 'DeepFlow 文档',
     },
   },
   shouldPrefetch: false,
@@ -46,34 +37,6 @@ module.exports = ({
     editLinks: true, // 启用编辑
     editLinkText: "编辑",
 
-    //*** 以下是Vdoing主题相关配置，文档：https://doc.xugaoyi.com/pages/a20ce8/ ***//
-
-    // category: false, // 是否打开分类功能，默认true
-    // tag: false, // 是否打开标签功能，默认true
-    // archive: false, // 是否打开归档功能，默认true
-    // categoryText: '随笔', // 碎片化文章（_posts文件夹的文章）预设生成的分类值，默认'随笔'
-
-    // bodyBgImg: [
-    //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175828.jpeg',
-    //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175845.jpeg',
-    //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175846.jpeg'
-    // ], // body背景大图，默认无。 单张图片 String | 多张图片 Array, 多张图片时每隔15秒换一张。
-    // bodyBgImgOpacity: 0.5, // body背景图透明度，选值 0.1~ 1.0, 默认0.5
-    // titleBadge: false, // 文章标题前的图标是否显示，默认true
-    // titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
-    //   '图标地址1',
-    //   '图标地址2'
-    // ],
-    // contentBgStyle: 1, // 文章内容块的背景风格，默认无. 1 方格 | 2 横线 | 3 竖线 | 4 左斜线 | 5 右斜线 | 6 点状
-
-    // updateBar: { // 最近更新栏
-    //   showToArticle: false, // 显示到文章页底部，默认true
-    //   moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
-    // },
-    // rightMenuBar: false, // 是否显示右侧文章大纲栏，默认true (屏宽小于1300px下无论如何都不显示)
-    // sidebarOpen: false, // 初始状态是否打开左侧边栏，默认true
-    // pageButton: false, // 是否显示快捷翻页按钮，默认true
-
     // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | <自定义>    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
     sidebar: "structuring",
 
@@ -91,29 +54,11 @@ module.exports = ({
   // 注入到页面<head>中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
   head: [
     ["link", { rel: "icon", href: "/img/favicon.ico" }], //favicons，资源放在public文件夹
-    [
-      "meta",
-      {
-        name: "keywords",
-        content:
-          "前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown",
-      },
-    ],
-    ["meta", { name: "baidu-site-verification", content: "7F55weZDDc" }], // 百度统计的站长验证（你可以去掉）
-    ["meta", { name: "theme-color", content: "#11a8cd" }], // 移动浏览器主题颜色
-    // [
-    //   'script',
-    //   {
-    //     src: 'https://cdn.bootcdn.net/ajax/libs/clipboard.js/2.0.9/clipboard.js',
-    //   },
-    // ],
+    ["meta", { name: "theme-color", content: "#0a72ef" }], // 移动浏览器主题颜色
   ],
 
   // 插件配置
   plugins: [
-    // require("./plugins/love-me/index.js"),
-    // 全文搜索。 ⚠️注意：此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。如在意初次访问速度的话可以不使用此插件！（推荐：vuepress-plugin-thirdparty-search）
-    // "fulltext-search",
     require("./plugins/fulltext-search/index.js"),
     [require("./plugins/one-click-copy/index.js"), {
       en: {
@@ -125,66 +70,12 @@ module.exports = ({
         toolTipMessage: "复制代码"
       }
     }],
-
-    // [
-    //   "one-click-copy", // 代码块复制按钮
-    //   {
-    //     copySelector: [
-    //       'div[class*="language-"] pre',
-    //       'div[class*="aside-code"] aside',
-    //     ], // String or Array
-    //     copyMessage: "复制成功", // default is 'Copy successfully and then paste it for use.'
-    //     duration: 1000, // prompt message display time.
-    //     showInMobile: false, // whether to display on the mobile side, default: false.
-    //   },
-    // ],
-
-    [
-      "demo-block", // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
-      {
-        settings: {
-          // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
-          // cssLib: ['http://xxx'], // 在线示例中的css依赖
-          // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
-          jsfiddle: false, // 是否显示 jsfiddle 链接
-          codepen: true, // 是否显示 codepen 链接
-          horizontal: false, // 是否展示为横向样式
-        },
-      },
-    ],
     [
       "vuepress-plugin-zooming", // 放大图片
       {
         selector: ".theme-vdoing-content img:not(.no-zoom)", // 排除class是no-zoom的图片
         options: {
           bgColor: "rgba(0,0,0,0.6)",
-        },
-      },
-    ],
-    // [
-    //   "vuepress-plugin-comment", // 评论
-    //   {
-    //     choosen: "gitalk",
-    //     options: {
-    //       clientID: "a6e1355287947096b88b",
-    //       clientSecret: "f0e77d070fabfcd5af95bebb82b2d574d7248d71",
-    //       repo: "blog-gitalk-comment", // GitHub 仓库
-    //       owner: "xugaoyi", // GitHub仓库所有者
-    //       admin: ["xugaoyi"], // 对仓库有写权限的人
-    //       // distractionFreeMode: true,
-    //       pagerDirection: "last", // 'first'正序 | 'last'倒序
-    //       id: "<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>", //  页面的唯一标识,长度不能超过50
-    //       title: "「评论」<%- frontmatter.title %>", // GitHub issue 的标题
-    //       labels: ["Gitalk", "Comment"], // GitHub issue 的标签
-    //       body: "页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>", // GitHub issue 的内容
-    //     },
-    //   },
-    // ],
-    [
-      "@vuepress/last-updated", // "上次更新"时间格式
-      {
-        transformer: (timestamp, lang) => {
-          return dayjs(timestamp).format("YYYY/MM/DD, HH:mm:ss");
         },
       },
     ],
@@ -220,7 +111,7 @@ module.exports = ({
   },
 
   // 监听文件变化并重新构建
-  extraWatchFiles: [".vuepress/config.ts"],
+  extraWatchFiles: ["./../.vuepress/config.js"],
 
   configureWebpack: {
     module: {
