caa141e1c45d6ee7f1293f75b2cf1528873b40c6
zhifu <zhifu@yunshan.net>
2024-06-17 18:28:40 +0800

    [Compiler]: fix auto-add-title-order plugin bug

--- docs/.vuepress/plugins/auto-add-title-order/index.js
+++ docs/.vuepress/plugins/auto-add-title-order/index.js
@@ -1,41 +1,48 @@
 // 自动补全title序号
 module.exports = function (md, config) {
-    const cacheTitleOrder = []
-    let latest = ''
-    md.renderer.rules.heading_open = (tokens, idx, options, env, self) => {
-        const { relativePath } = env
-        if (!relativePath) {
-            return self.renderToken(tokens, idx, options, env);
-        }
-        if (relativePath !== latest) {
-            latest = relativePath
-            cacheTitleOrder.splice(0, cacheTitleOrder.length)
-        }
-        /**
-         * 找到这个，然后把idx+1的xx内容修改为1.xx
-         * 1. 找到token，并解析出级别，级别需要-1，从0开始
-         * 2. 如果当前级别存在，则+1，同时清空cacheTitleIndex leavel后面的数据;不存在则赋值为1
-         */
-        const token = tokens[idx]
-        let [, leavel] = token.tag.split('')
-        leavel = leavel - 1; // 
-        if (cacheTitleOrder[leavel]) {
-            cacheTitleOrder[leavel] = cacheTitleOrder[leavel] + 1
-            // 后面的清除掉
-            cacheTitleOrder.splice(leavel + 1, cacheTitleOrder.length - leavel)
-        } else {
-            do {
-                cacheTitleOrder[leavel--] = 1
-            } while (!cacheTitleOrder[leavel] && leavel >= 0)
-        }
-
-        const _cacheTitleOrder = cacheTitleOrder.filter(Boolean) // [null, 1]
-
-        const currentLeavel = _cacheTitleOrder.join('.') + (_cacheTitleOrder.length === 1 ? '. ' : ' ') // 1. 2.2
-        const nextToken = tokens[idx + 1]
-        nextToken.content = currentLeavel + nextToken.content
-        nextToken.children[nextToken.children.length - 1].content = currentLeavel + nextToken.children[nextToken.children.length - 1].content
-
-        return self.renderToken(tokens, idx, options, env);
+  const cacheTitleOrder = [];
+  let latest = "";
+  md.renderer.rules.heading_open = (tokens, idx, options, env, self) => {
+    const { relativePath } = env;
+    if (!relativePath) {
+      return self.renderToken(tokens, idx, options, env);
     }
-}
+    if (relativePath !== latest) {
+      latest = relativePath;
+      cacheTitleOrder.splice(0, cacheTitleOrder.length);
+    }
+    /**
+     * 找到这个，然后把idx+1的xx内容修改为1.xx
+     * 1. 找到token，并解析出级别，级别需要-1，从0开始
+     * 2. 如果当前级别存在，则+1，同时清空cacheTitleIndex leavel后面的数据;不存在则赋值为1
+     */
+    const token = tokens[idx];
+    let [, leavel] = token.tag.split("");
+    leavel = leavel - 1; //
+    if (cacheTitleOrder[leavel]) {
+      cacheTitleOrder[leavel] = cacheTitleOrder[leavel] + 1;
+      // 后面的清除掉
+      cacheTitleOrder.splice(leavel + 1, cacheTitleOrder.length - leavel);
+    } else {
+      do {
+        cacheTitleOrder[leavel--] = 1;
+      } while (!cacheTitleOrder[leavel] && leavel >= 0);
+    }
+
+    const _cacheTitleOrder = cacheTitleOrder.filter(Boolean); // [null, 1]
+
+    const currentLeavel =
+      _cacheTitleOrder.join(".") + (_cacheTitleOrder.length === 1 ? ". " : " "); // 1. 2.2
+    const nextToken = tokens[idx + 1];
+    nextToken.content = currentLeavel + nextToken.content;
+
+    // 如果标题中出现了 `` 会有格式问题
+    // 找到第一个text的即可
+    const index = nextToken.children.findIndex(c=>c.type === 'text')
+
+    nextToken.children[index].content =
+      currentLeavel + nextToken.children[index].content;
+
+    return self.renderToken(tokens, idx, options, env);
+  };
+};
