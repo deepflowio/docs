572c20322ccaac3aa1b433e31bfff47dd5bcfae2
zhifu <zhifu@yunshan.net>
2024-06-28 15:18:56 +0800

    [Compiler]: Replace image scaling plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -88,15 +88,6 @@ module.exports = {
       },
     ],
     [
-      require("./plugins/vuepress-plugin-zooming/index.js"), // 放大图片
-      {
-        selector: ".theme-vdoing-content img:not(.no-zoom)", // 排除class是no-zoom的图片
-        options: {
-          bgColor: "rgba(0,0,0,0.6)",
-        },
-      },
-    ],
-    [
       require("./plugins/mermaidjs/index.js"),
       {
         themeVariables: {
