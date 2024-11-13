7ea3aee6a7fbd66f8197bcb818724926d6db0d93
zhifu <zhifu@yunshan.net>
2024-11-13 10:23:26 +0800

    compiler: fix mobile hash bug

--- vdoing/components/SidebarLinks.vue
+++ vdoing/components/SidebarLinks.vue
@@ -15,82 +15,111 @@
 </template>
 
 <script>
-import SidebarGroup from '@theme/components/SidebarGroup.vue'
-import SidebarLink from '@theme/components/SidebarLink.vue'
-import { isActive } from '../util'
+import SidebarGroup from "@theme/components/SidebarGroup.vue";
+import SidebarLink from "@theme/components/SidebarLink.vue";
+import { isActive } from "../util";
+import _ from "lodash";
 
 export default {
-  name: 'SidebarLinks',
+  name: "SidebarLinks",
 
   components: { SidebarGroup, SidebarLink },
 
   props: [
-    'items',
-    'depth',  // depth of current sidebar links
-    'sidebarDepth', // depth of headers to be extracted
-    'initialOpenGroupIndex'
+    "items",
+    "depth", // depth of current sidebar links
+    "sidebarDepth", // depth of headers to be extracted
+    "initialOpenGroupIndex",
   ],
 
-  data () {
+  data() {
     return {
-      openGroupIndex: this.initialOpenGroupIndex || 0
-    }
+      openGroupIndex: this.initialOpenGroupIndex || 0,
+      isMQMobile: false,
+    };
   },
 
-  created () {
-    this.refreshIndex()
+  beforeMount() {
+    this.isMQMobile = window.innerWidth < 720 ? true : false;
+
+    window.addEventListener("resize", () => {
+      this.isMQMobile = window.innerWidth < 720 ? true : false;
+    });
+  },
+  created() {
+    this.refreshIndex();
+  },
+
+  mounted() {
+    const { hash } = this.$route;
+    if (hash) {
+      this.listenHash(hash);
+    }
   },
 
   watch: {
-    '$route' () {
-      this.refreshIndex()
-    }
+    $route() {
+      this.refreshIndex();
+    },
+    "$route.hash"(n) {
+      this.listenHash(n);
+    },
   },
 
   methods: {
-    refreshIndex () {
-      const index = resolveOpenGroupIndex(
-        this.$route,
-        this.items
-      )
-      if (index > -1) {
-        this.openGroupIndex = index
+    listenHash(n) {
+      if (this.isMQMobile && n.indexOf(".")) {
+        this.scrollToElementById(n.replace(/\./g, "\\."));
       }
     },
-
-    toggleGroup (index) {
-      this.openGroupIndex = index === this.openGroupIndex ? -1 : index
-    },
-
-    isActive (page) {
-      return isActive(this.$route, page.regularPath)
-    }
-  }
-}
-
-function resolveOpenGroupIndex (route, items) {
-  for (let i = 0; i < items.length; i++) {
-    const item = items[i]
-    if (descendantIsActive(route, item)) {
-      return i
-    }
-  }
-  return -1
-}
-
-function descendantIsActive (route, item) {
-  if (item.type === 'group') {
-    if(item.path === route.path){
-      return true
-    }
-    return item.children.some(child => {
-      if (child.type === 'group') {
-        return descendantIsActive(route, child)
+    scrollToElementById: _.debounce((id) => {
+      const element = document.querySelector(id);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth", block: "start" });
       } else {
-        return child.type === 'page' && isActive(route, child.path)
+        console.error(`没有找到 ID 为 ${id} 的元素`);
       }
-    })
+    }, 100),
+    refreshIndex() {
+      const index = resolveOpenGroupIndex(this.$route, this.items);
+      if (index > -1) {
+        this.openGroupIndex = index;
+      }
+    },
+
+    toggleGroup(index) {
+      this.openGroupIndex = index === this.openGroupIndex ? -1 : index;
+    },
+
+    isActive(page) {
+      return isActive(this.$route, page.regularPath);
+    },
+  },
+};
+
+function resolveOpenGroupIndex(route, items) {
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    if (descendantIsActive(route, item)) {
+      return i;
+    }
   }
-  return false
+  return -1;
+}
+
+function descendantIsActive(route, item) {
+  if (item.type === "group") {
+    if (item.path === route.path) {
+      return true;
+    }
+    return item.children.some((child) => {
+      if (child.type === "group") {
+        return descendantIsActive(route, child);
+      } else {
+        return child.type === "page" && isActive(route, child.path);
+      }
+    });
+  }
+  return false;
 }
 </script>
